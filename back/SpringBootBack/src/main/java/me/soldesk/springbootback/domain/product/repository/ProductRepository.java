e0713edf8df4062e2dfc0a86ac7ce0f5034c50e9
shin <wogud815@gmail.com>
2026-07-09 18:43:47 +0900

    Update ProductRepository.java

--- back/SpringBootBack/src/main/java/me/soldesk/springbootback/domain/product/repository/ProductRepository.java
+++ back/SpringBootBack/src/main/java/me/soldesk/springbootback/domain/product/repository/ProductRepository.java
@@ -4,8 +4,14 @@ import me.soldesk.springbootback.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByCategoryId(Long categoryId);
+    Optional<Product> findFirstByProductNameContainingAndProductStatusAndStockQuantityGreaterThanOrderByPriceAsc(
+            String productName,
+            String productStatus,
+            Integer stockQuantity
+    );
 }
